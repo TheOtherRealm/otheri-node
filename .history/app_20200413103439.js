096f434ce3f9eb1427c0820efaee6688afe739f7
Other Realm <the@otherrealm.org>
2020-04-13 13:43:24 -0400

    Working on stuff

--- .history/app_20200413103439.js
+++ .history/app_20200413103439.js
@@ -1,10 +1,12 @@
-const http = require('http')
+const https = require('https')
+
 const data = JSON.stringify({
 	todo: 'Buy the milk'
 })
+
 const options = {
-	hostname: 'localhost',
-	port: 8080,
+	hostname: 'otherrealm.org',
+	port: 443,
 	path: '/todos',
 	method: 'POST',
 	headers: {
@@ -12,14 +14,18 @@ const options = {
 		'Content-Length': data.length
 	}
 }
-const req = http.request(options, res => {
+
+const req = https.request(options, res => {
 	console.log(`statusCode: ${res.statusCode}`)
+
 	res.on('data', d => {
 		process.stdout.write(d)
 	})
 })
+
 req.on('error', error => {
 	console.error(error)
 })
+
 req.write(data)
 req.end()
